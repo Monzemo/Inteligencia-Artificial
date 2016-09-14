8abd38c03a56354d399adec5af089277a33ac887
Monzemo <monzemo@gmail.com>
2016-09-13 22:07:54 -0700

    Practicas

---
+++ Insercion.py
@@ -0,0 +1,17 @@
+from time import clock
+
+lista = [3,1,8,2,5]
+
+tiempo_inicial = clock()
+for i in range(1,len(lista)):
+	temp = lista[j]
+	j = i -1
+	while j >= 0:
+		if temp < lista[j]:
+		lista[j+1] = lista[j]
+		lista[j = temp
+		j = j-1
+tiempo_final = clock()
+			
+print (lista)
+print (tiempo_final - tiempo_inicial)
